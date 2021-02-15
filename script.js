1bf3793c4f3131746d79ec41b526ee5ff534e4de
MHinderer <hinderer98@gmail.com>
2021-02-16 00:28:41 +0100

    Programm funktioniert (fast)
    Player seine first click Karte dreht sich nicht mehr um = MUSS BEHOBEN WERDEN
    
    Player kann eine Karte zweimal klicken = DARF NICHT SEIN

--- script.js
+++ script.js
@@ -1,114 +1,239 @@
-//Karten Array
+//Variablen definieren und das DOM Element verknüpfen
+var menü = document.querySelector(".menü");
+var grid = document.querySelector(".grid");
+var scoreComputer = document.querySelector(".scoreComputer");
+var scoreUser = document.querySelector(".scoreUser");
+var gamefield = document.querySelector(".gamefield");
+var easyButton = document.getElementById("easyButton");
+var middleButton = document.getElementById("middleButton");
+var hardButton = document.getElementById("hardButton");
+var winMessage = document.querySelector(".winMessage");
+var replay = document.querySelector("#replay");
+var afterGame = document.querySelector("#afterGame");
+//Boolean Werte
+var playerBo = false;
+var computerBo = true;
+easyButton.addEventListener("click", function () {
+    gamefield.setAttribute("style", "display:unset");
+    cardGenerator();
+    setTimeout(function () {
+        computer();
+    }, 1000);
+    easyButton.disabled = true;
+});
+//leere Arrays
+var openCards = [];
+var hiddenArray = [];
+var scoreComputerArray = [];
+var scorePlayerArray = [];
+//Karten Array LEICHT
 var cards = [
     {
         icon: "fas fa-cat",
         colour: "#F78181",
-        text: ""
+        text: "",
+        key: "karte1"
     },
     {
         icon: "fas fa-cat",
         colour: "#F78181",
-        text: ""
+        text: "",
+        key: "karte2"
     },
     {
         icon: "fas fa-dragon",
         colour: "#F5DA81",
-        text: ""
+        text: "",
+        key: "karte3"
     },
     {
         icon: "fas fa-dragon",
         colour: "#F5DA81",
-        text: ""
+        text: "",
+        key: "karte4"
     },
     {
         icon: "fas fa-fish",
         colour: "#81F781",
-        text: ""
+        text: "",
+        key: "karte5"
     },
     {
         icon: "fas fa-fish",
         colour: "#81F781",
-        text: ""
+        text: "",
+        key: "karte6"
     },
     {
         icon: "fas fa-horse",
         colour: "#819FF7",
-        text: ""
+        text: "",
+        key: "karte7"
     },
     {
         icon: "fas fa-horse",
         colour: "#819FF7",
-        text: ""
+        text: "",
+        key: "karte8"
     }
 ];
-//Variablen definieren und das DOM Element verknüpfen
-var grid = document.querySelector(".grid");
-var scoreComputer = document.querySelector(".scoreComputer");
-var scoreUser = document.querySelector(".scoreUser");
-var _loop_1 = function (index) {
-    var card = document.createElement("div");
-    card.classList.add("card");
-    card.style.backgroundColor = "grey";
-    card.addEventListener("click", function () {
-        card.innerHTML = "<i class='" + cards[index].icon + "'></i>";
-        card.style.backgroundColor = cards[index].colour;
-        setTimeout(function () {
-            card.innerHTML = "";
-            card.style.backgroundColor = "grey";
-        }, 1000);
-    });
-    grid.appendChild(card);
-};
-//Schleife die das Grid mit den Array Objekten befüllt. Erzeugtes DIVElement  bekommt die Objekte aus dem Array, als auch eine Hintergrundfarbe die als Rückseite erscheint. 
-for (var index = 0; index < cards.length; index++) {
-    _loop_1(index);
+//Computer
+//Kann zufällige Indexe aus meinem Kartenarray wählen und ausgeben.
+//pusht die random Indexe in ein leeres randomArray.
+//in diesem werden die zwei Indexe verglichen ob sie gleich sind oder nicht.
+//wenn diese gleich sind wird die function nocheinmal aufgerufen, bis die if-condition erfüllt ist.
+//somit kann der Computer nicht eine Karten zwei mal aufdecken
+function computer() {
+    setTimeout(function () {
+        var random1 = Math.floor(Math.random() * cards.length);
+        var random2 = Math.floor(Math.random() * cards.length);
+        var randomCard1 = cards[random1];
+        var randomCard2 = cards[random2];
+        var card1 = document.querySelector(".card" + random1);
+        var card2 = document.querySelector(".card" + random2);
+        console.log(random1);
+        console.log(random2);
+        var randomArray = [];
+        randomArray.push(random1);
+        randomArray.push(random2);
+        console.log(randomArray);
+        if (randomArray[0] != randomArray[1]) {
+            if (card1.classList.contains("hidden")) {
+                computer();
+            }
+            else if (card2.classList.contains("hidden")) {
+                computer();
+            }
+            else {
+                element(random1);
+                element(random2);
+                time(random1);
+                time(random2);
+                randomArray = [];
+            }
+        }
+        else {
+            computer();
+        }
+    }, 200);
+}
+function time(index) {
+    var card = document.querySelector(".card" + index);
+    setTimeout(function () {
+        card.innerHTML = "";
+        card.style.backgroundColor = "grey";
+    }, 1000);
+}
+//Schleife die das Grid mit den Array Objekten befüllt. Erzeugtes DIVElement  bekommt die Objekte aus dem Array, als auch eine Hintergrundfarbe die als Rückseite erscheint.
+function cardGenerator() {
+    var _loop_1 = function (index) {
+        var card = document.createElement("div");
+        card.classList.add("card");
+        card.classList.add("card" + index);
+        card.style.backgroundColor = "grey";
+        grid.appendChild(card);
+        card.addEventListener("click", function () {
+            if (playerBo == true) {
+                computerBo = false;
+                element(index);
+            }
+            if (hiddenArray.length == 2) {
+                setTimeout(function () {
+                    card.innerHTML = "";
+                    card.style.backgroundColor = "grey";
+                }, 1000);
+            }
+        });
+    };
+    for (var index = 0; index < cards.length; index++) {
+        _loop_1(index);
+    }
+}
+function element(index) {
+    var card = document.querySelector(".card" + index);
+    card.innerHTML = "<i class='" + cards[index].icon + "'></i>";
+    card.style.backgroundColor = cards[index].colour;
+    openCards.push(cards[index].colour);
+    openCards.push(cards[index].key);
+    console.log(openCards);
+    hiddenArray.push(card);
+    console.log(hiddenArray);
+    addClickedCards(index);
 }
 //Erzeugte DIVs (Karten) werden beim neu laden der Seite/Spiels durcheinander angeordnet. Dabei bekommen die Index Plätze random die Werte zugeschrieben.
 function randomizeArray() {
     cards.sort(function () { return 0.5 - Math.random(); });
 }
 randomizeArray();
-// Array für die zwei geklickten Karten der Spieler. In diesem Array werden die Karten verglichen ob sie ein Paar sind oder nicht.
-//var openedCards: Value[] = [];
-//
-//grid.addEventListener("click", function (event): void {
-//    const clickTarget = event.target;
-//    console.log(clickTarget);
-//    if (clickTarget.classList.contains("card") && !openedCards.includes(clickTarget)) {
-//        addClickedCards(clickTarget);
-//    }
-//});
-//
-//function addClickedCards(clickedTarget): void {
-//    openedCards.push();
-//    if (openedCards.length === 2) {
-//        matchingCards();
-//    }
-//}
-//
-//function matchingCards(): void {
-//    if (openedCards[0].firstElementChild.className === openedCards[1].firstElementChild.className) {
-//        console.log("we have a match");
-//        openedCards = [];
-//        endGame();
-//    } else {
-//        console.log(openedCards[0].firstElementChild.classList);
-//        console.log(openedCards[1].firstElementChild.classList);
-//        console.log("no match");
-//        setTimeout(function (): void {
-//            openedCards[0].classList.toggle("open");
-//            openedCards[0].classList.toggle("show");
-//            openedCards[1].classList.toggle("open");
-//            openedCards[1].classList.toggle("show");
-//            openedCards = [];
-//        },         1000);
-//    }
-//}
-// Karten vergleichen
-//const checkMatch: string = (icons) => {
-//    if (icons[0] === icons[1]) {
-//        console.log("it's a match");
-//        return true;
-//    }
-//};
+function addClickedCards(index) {
+    if (openCards.length == 4) {
+        matchingCards();
+    }
+}
+//if/else
+function matchingCards() {
+    setTimeout(function () {
+        if (openCards[0] == openCards[2] && openCards[1] != openCards[3]) {
+            hiddenArray[0].classList.add("hidden");
+            hiddenArray[1].classList.add("hidden");
+            console.log(openCards[0] == openCards[2] && openCards[1] != openCards[3]);
+            openCards = [];
+            hiddenArray = [];
+            if (cards.length != 0 && computerBo == true) {
+                scoreComputerArray.push(1);
+                counter();
+                computer();
+                hiddenArray = [];
+                openCards = [];
+            }
+            if (playerBo == true) {
+                scorePlayerArray.push(1);
+                counter();
+                hiddenArray = [];
+                openCards = [];
+            }
+        }
+        else {
+            if (computerBo == true && playerBo == false) {
+                setTimeout(function () {
+                    playerBo = true;
+                }, 1000);
+                hiddenArray = [];
+                openCards = [];
+            }
+            else if (computerBo == false && playerBo == true) {
+                setTimeout(function () {
+                    computerBo = true;
+                    hiddenArray = [];
+                    openCards = [];
+                    console.log(hiddenArray);
+                    console.log(openCards);
+                    playerBo = false;
+                    computer();
+                }, 1000);
+            }
+        }
+    }, 500);
+}
+//Zähler
+function counter() {
+    var res = scorePlayerArray.length + scoreComputerArray.length;
+    scoreComputer.innerHTML = "Score" + scoreComputerArray.length;
+    scoreUser.innerHTML = "Score" + scorePlayerArray.length;
+    if (res == 4) {
+        afterGame.setAttribute("style", "display:unset");
+        gamefield.setAttribute("style", "display:none");
+        if (scoreComputerArray.length > scorePlayerArray.length) {
+            winMessage.innerHTML = "YOU LOSE";
+        }
+        else if (scoreComputerArray.length == scorePlayerArray.length) {
+            winMessage.innerHTML = "DRAW";
+        }
+    }
+}
+//Replay: click function auf dem Button lässt die Seite neu laden.
+//Dadurch gelangen wir wieder auf die "Startseite" bzw. Anfangssituation.
+replay.addEventListener("click", function () {
+    location.reload();
+});
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,143 +1,284 @@
+//Variablen definieren und das DOM Element verknüpfen
+var menü: HTMLDivElement = document.querySelector(".menü");
+var grid: HTMLElement = document.querySelector(".grid");
+var scoreComputer: HTMLSpanElement = document.querySelector(".scoreComputer");
+var scoreUser: HTMLSpanElement = document.querySelector(".scoreUser");
+var gamefield: HTMLDivElement = document.querySelector(".gamefield");
+var easyButton: HTMLButtonElement = (document.getElementById("easyButton") as HTMLButtonElement);
+var middleButton: HTMLButtonElement = (document.getElementById("middleButton") as HTMLButtonElement);
+var hardButton: HTMLButtonElement = (document.getElementById("hardButton") as HTMLButtonElement);
+var winMessage: HTMLElement = document.querySelector(".winMessage");
+var replay: HTMLButtonElement = document.querySelector("#replay");
+var afterGame: HTMLDivElement = document.querySelector("#afterGame");
+
+//Boolean Werte
+var playerBo: boolean = false;
+var computerBo: boolean = true;
+
+easyButton.addEventListener("click", function(): void {
+    gamefield.setAttribute("style", "display:unset");
+    cardGenerator();
+    setTimeout (() => {
+        computer();
+    },          1000);
+    easyButton.disabled = true;
+});
+
+
+
 //Karten Interface
-interface Value {
+interface Wearth {
     icon: string;
     colour: string;
     text: string;
+    key: string;
 }
 
-//Karten Array
-var cards: Value[] = [
+//leere Arrays
+var openCards: string[] = [];
+var hiddenArray: HTMLDivElement[] = [];
+var scoreComputerArray: number [] = [];
+var scorePlayerArray: number [] = [];
+
+//Karten Array LEICHT
+var cards: Wearth[] = [
     {
         icon: "fas fa-cat",
         colour: "#F78181",
-        text: ""
+        text: "",
+        key: "karte1"
     },
     {
         icon: "fas fa-cat",
         colour: "#F78181",
-        text: ""
+        text: "",
+        key: "karte2"
     },
     {
         icon: "fas fa-dragon",
         colour: "#F5DA81",
-        text: ""
+        text: "",
+        key: "karte3"
     },
     {
         icon: "fas fa-dragon",
         colour: "#F5DA81",
-        text: ""
+        text: "",
+        key: "karte4"
     },
     {
         icon: "fas fa-fish",
         colour: "#81F781",
-        text: ""
+        text: "",
+        key: "karte5"
     },
     {
         icon: "fas fa-fish",
         colour: "#81F781",
-        text: ""
+        text: "",
+        key: "karte6"
     },
     {
         icon: "fas fa-horse",
         colour: "#819FF7",
-        text: ""
+        text: "",
+        key: "karte7"
     },
     {
         icon: "fas fa-horse",
         colour: "#819FF7",
-        text: ""
+        text: "",
+        key: "karte8"
     }
 ];
 
+//Computer
+//Kann zufällige Indexe aus meinem Kartenarray wählen und ausgeben.
+//pusht die random Indexe in ein leeres randomArray.
+//in diesem werden die zwei Indexe verglichen ob sie gleich sind oder nicht.
+//wenn diese gleich sind wird die function nocheinmal aufgerufen, bis die if-condition erfüllt ist.
+//somit kann der Computer nicht eine Karten zwei mal aufdecken
+function computer(): void {
+    setTimeout (() => {
+    const random1: number = Math.floor(Math.random() * cards.length);
+    const random2: number = Math.floor(Math.random() * cards.length);
+    const randomCard1: Wearth = cards[random1];
+    const randomCard2: Wearth = cards[random2];
 
-//Variablen definieren und das DOM Element verknüpfen
-var grid: HTMLElement = document.querySelector(".grid"); 
-var scoreComputer: HTMLSpanElement = document.querySelector(".scoreComputer"); 
-var scoreUser: HTMLSpanElement = document.querySelector(".scoreUser"); 
+    let card1: HTMLDivElement = document.querySelector(".card" + random1);
+    let card2: HTMLDivElement = document.querySelector(".card" + random2);
 
-//Schleife die das Grid mit den Array Objekten befüllt. Erzeugtes DIVElement  bekommt die Objekte aus dem Array, als auch eine Hintergrundfarbe die als Rückseite erscheint. 
+    console.log(random1);
+    console.log(random2);
+
+    var randomArray: number[] = [];
+    randomArray.push(random1);
+    randomArray.push(random2);
+
+    console.log(randomArray);
+
+    if (randomArray[0] != randomArray[1]) {
+        if ( card1.classList.contains("hidden")) {
+            computer();
+        } else if (card2.classList.contains("hidden")) {
+            computer();
+        } else {
+            element(random1);
+            element(random2);
+            time(random1);
+            time(random2);
+            randomArray = [];
+        }
+        
+    } else {
+        computer();
+    }
+    },          200);
+
+}
+
+function time(index: number): void {
+    let card: HTMLDivElement = document.querySelector(".card" + index);
+    setTimeout (() => {
+        card.innerHTML = "";
+        card.style.backgroundColor = "grey";
+    },          1000);
+}
+
+
+
+
+//Schleife die das Grid mit den Array Objekten befüllt. Erzeugtes DIVElement  bekommt die Objekte aus dem Array, als auch eine Hintergrundfarbe die als Rückseite erscheint.
+function cardGenerator(): void { 
 for (let index: number = 0; index < cards.length; index++) {
     let card: HTMLDivElement = document.createElement("div");
     card.classList.add("card");
+    card.classList.add("card" + index);
+    
     card.style.backgroundColor = "grey";
-    card.addEventListener("click", function(): void {
-        card.innerHTML = "<i class='" + cards[index].icon  + "'></i>"; 
-        card.style.backgroundColor = cards[index].colour;
-        setTimeout(() => {
-            card.innerHTML = "";
-            card.style.backgroundColor = "grey";
-        },         1000);
-    });
+
     grid.appendChild(card);
+
+    card.addEventListener("click", function (): void {
+        if (playerBo == true) {
+            computerBo = false;
+            element(index);
+        }
+        if ( hiddenArray.length == 2) {
+            setTimeout(() => {
+                card.innerHTML = "";
+                card.style.backgroundColor = "grey";
+            },         1000);
+        }
+    });
+}
 }
 
+function element(index: number): void {
+    let card: HTMLDivElement = document.querySelector(".card" + index);
+    card.innerHTML = "<i class='" + cards[index].icon + "'></i>";
+    card.style.backgroundColor = cards[index].colour;
+
+    openCards.push(cards[index].colour);
+    openCards.push(cards[index].key);
+    console.log(openCards);
+
+    hiddenArray.push(card);
+    console.log(hiddenArray);
+
+    addClickedCards(index);
+}
+
+
 //Erzeugte DIVs (Karten) werden beim neu laden der Seite/Spiels durcheinander angeordnet. Dabei bekommen die Index Plätze random die Werte zugeschrieben.
-function randomizeArray(): void { 
+function randomizeArray(): void {
     cards.sort(() => 0.5 - Math.random());
-    }
+}
 
 randomizeArray();
 
 
 
+function addClickedCards(index: number): void {
+    if (openCards.length == 4) {
+        matchingCards();
+    }
+}
 
 
+//if/else
+function matchingCards(): void {
+    setTimeout(() => {
+    if (openCards[0] == openCards[2] && openCards[1] != openCards[3]) {
+        hiddenArray[0].classList.add("hidden");
+        hiddenArray[1].classList.add("hidden");
+        console.log(openCards[0] == openCards[2] && openCards[1] != openCards[3]);
+        
+        openCards = [];
+        hiddenArray = [];
+       
+        if (cards.length != 0 && computerBo == true) {
+            scoreComputerArray.push(1);
+            counter();
+            computer();
+            hiddenArray = [];
+            openCards = [];
+        }
+
+        if (playerBo == true) {
+            scorePlayerArray.push(1);
+            counter();
+            hiddenArray = [];
+            openCards = [];
+        }
+
+    } else {
+        
+        if ( computerBo == true && playerBo == false) {
+            setTimeout(() => {
+                playerBo = true;
+            },         1000);
+            hiddenArray = [];
+            openCards = [];
+     
+        } else if (computerBo == false && playerBo == true) {
+            setTimeout(() => {
+                computerBo = true;
+                hiddenArray = [];
+                openCards = [];
+
+                console.log(hiddenArray);
+                console.log(openCards);
+
+                playerBo = false;
+                computer();
+            },         1000);
+        }
+    }
+},             500);
+}
 
 
+//Zähler
+function counter(): void {
+    var res: number = scorePlayerArray.length + scoreComputerArray.length;
+    scoreComputer.innerHTML = "Score" + scoreComputerArray.length;
+    scoreUser.innerHTML = "Score" + scorePlayerArray.length;
 
+    if ( res == 4) {
+        afterGame.setAttribute("style", "display:unset");
+        gamefield.setAttribute("style", "display:none");
+        if (scoreComputerArray.length > scorePlayerArray.length) {
+            winMessage.innerHTML = "YOU LOSE";
+        } else if (scoreComputerArray.length == scorePlayerArray.length) {
+            winMessage.innerHTML = "DRAW";
+        }
 
+    }
+}
 
-
-
-
-
-
-
-
-// Array für die zwei geklickten Karten der Spieler. In diesem Array werden die Karten verglichen ob sie ein Paar sind oder nicht.
-//var openedCards: Value[] = [];
-//
-//grid.addEventListener("click", function (event): void {
-//    const clickTarget = event.target;
-//    console.log(clickTarget);
-//    if (clickTarget.classList.contains("card") && !openedCards.includes(clickTarget)) {
-//        addClickedCards(clickTarget);
-//    }
-//});
-//
-//function addClickedCards(clickedTarget): void {
-//    openedCards.push();
-//    if (openedCards.length === 2) {
-//        matchingCards();
-//    }
-//}
-//
-//function matchingCards(): void {
-//    if (openedCards[0].firstElementChild.className === openedCards[1].firstElementChild.className) {
-//        console.log("we have a match");
-//        openedCards = [];
-//        endGame();
-//    } else {
-//        console.log(openedCards[0].firstElementChild.classList);
-//        console.log(openedCards[1].firstElementChild.classList);
-//        console.log("no match");
-//        setTimeout(function (): void {
-//            openedCards[0].classList.toggle("open");
-//            openedCards[0].classList.toggle("show");
-//            openedCards[1].classList.toggle("open");
-//            openedCards[1].classList.toggle("show");
-//            openedCards = [];
-//        },         1000);
-//    }
-//}
-
-
-
-
-// Karten vergleichen
-//const checkMatch: string = (icons) => {
-//    if (icons[0] === icons[1]) {
-//        console.log("it's a match");
-//        return true;
-//    }
-//};
+//Replay: click function auf dem Button lässt die Seite neu laden.
+//Dadurch gelangen wir wieder auf die "Startseite" bzw. Anfangssituation.
+replay.addEventListener("click", function (): void {
+    location.reload();
+});
